bf0be15bb9890c2114a81b5510e842bc0c09b8c7
YCYLSTUDIO <157963447+YCYLSTUDIO@users.noreply.github.com>
2024-01-30 20:07:19 +0700

    Add files via upload

--- js & css/index 2.js
+++ js & css/index 2.js
@@ -756,28 +756,28 @@ function logout() {
 }
 
 setInterval(() => {
-    // console.clear();
+    console.clear();
 }, 20);
 
 
-// // Disable right-click
-// document.addEventListener('contextmenu', (e) => e.preventDefault());
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
 
-// function ctrlShiftKey(e, keyCode) {
-//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-// }
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
 
-// document.onkeydown = (e) => {
-//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-//   if (
-//     event.keyCode === 123 ||
-//     ctrlShiftKey(e, 'I') ||
-//     ctrlShiftKey(e, 'J') ||
-//     ctrlShiftKey(e, 'C') ||
-//     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-//   )
-//     return false;
-// };
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
 
 setTimeout(function() {
     const bgfull = document.getElementById("bgfull");
